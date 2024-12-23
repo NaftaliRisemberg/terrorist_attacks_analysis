ae69aa38f2ab02f528ed830517871c17da1fc4e0
NaftaliRisemberg <n9742686@gmail.com>
2024-12-23 18:38:26 +0200

    added a function to get the most common attack type or groups using the same attack type, with city or country filters

--- blueprints/attack_type_bp.py
+++ blueprints/attack_type_bp.py
@@ -3,7 +3,7 @@ from services import *
 
 attack_type_bp = Blueprint('attack_type_bp', __name__)
 
-@attack_type_bp.route("/most_attack_type", methods=['GET'])
+@attack_type_bp.route("/most_severe_attack_types", methods=['GET'])
 def get_attack_types():
     count = request.args.get('count', type=int)
     try:
@@ -17,4 +17,27 @@ def get_attack_types():
     except Exception as e:
         print(f"Error: {e}")
 
+@attack_type_bp.route('/groups_with_shared_attack_typs', methods=['GET'])
+def get_groups_with_shared_attack_typs():
+    region = request.args.get('region', type=str)
+    country = request.args.get('country', type=str)
+    try:
+        data = find_groups_with_shared_attack_types(region, country)
 
+        all_groups = [{
+            'terror group': row[0],
+            'num attack types': row[1]
+        } for row in data[0]]
+
+        most_attacked_attack_types = {
+            'attack type': data[1][0],
+            'number groups': data[1][1]  #
+        }
+
+        return jsonify({
+            'groups_with_shared_attack_types': all_groups,
+            'attack_type_with_most_groups': most_attacked_attack_types
+        })
+
+    except Exception as e:
+        print(f"Error: {e}")
--- services/attacke_service.py
+++ services/attacke_service.py
@@ -16,3 +16,58 @@ def get_most_attack_types(count):
         .all()
     )
     return data
+
+def find_groups_with_shared_attack_types(region=None, country=None):
+    all_groups = find_all_groups_with_shared_attack_types(region, country)
+    most_attacked_attack = find_attack_typs_with_most_groups(region, country)
+    return all_groups, most_attacked_attack
+
+def find_all_groups_with_shared_attack_types(region=None, country=None):
+    attack_type_count = func.count(Attack.terror_group_id).label('attack_type_count')
+
+    query = (
+        session.query(
+            TerrorGroup.gang_name,
+            attack_type_count
+        )
+        .join(TerrorGroup.attacks)
+        .join(Attack.locations)
+    )
+
+    if region:
+        query = query.filter(Location.region == region)
+    if country:
+        query = query.filter(Location.country == country)
+
+    data = (
+        query
+        .group_by(TerrorGroup.gang_name)
+        .having(attack_type_count > 1)
+        .all()
+    )
+    return data
+
+def find_attack_typs_with_most_groups(region=None, country=None):
+    groups_count = func.count(Attack.terror_group_id).label('num_groups')
+
+    query = (
+        session.query(
+            AttackType.attack_type,
+            groups_count
+        )
+        .join(Attack.attack_types)
+        .join(Attack.locations)
+    )
+
+    if region:
+        query = query.filter(Location.region == region)
+    if country:
+        query = query.filter(Location.country == country)
+
+    data = (
+        query
+        .group_by(AttackType.attack_type)
+        .order_by(groups_count.desc())
+        .first()
+    )
+    return data
