9d76e37fcc826a0386fd62b947a2e17a7ccd95e2
NaftaliRisemberg <n9742686@gmail.com>
2024-12-23 15:54:32 +0200

    added imports

--- blueprints/__init__.py
+++ blueprints/__init__.py
@@ -1,3 +1,4 @@
 from .attack_type_bp import *
 from .region_bp import *
 from .terror_groups_bp import *
+from .target_type_bp import *
--- services/__init__.py
+++ services/__init__.py
@@ -1,3 +1,4 @@
 from .attacke_service import get_most_attack_types
 from .region_service import calculate_average_victims_per_event_by_region, calculate_percent_change
 from .terror_groups_service import get_most_damaging_terror_groups, calculate_most_active_groups
+from .target_type_service import calculate_groups_with_shared_targets
