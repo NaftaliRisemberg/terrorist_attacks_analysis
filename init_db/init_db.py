e9dfd512cee4b2eea54a7c51701fad36b6a59030
NaftaliRisemberg <n9742686@gmail.com>
2024-12-24 18:01:39 +0200

    changed init_db to work with CSV files of the two formats

--- init_db/init_db.py
+++ init_db/init_db.py
@@ -1,73 +1,16 @@
-import pandas as pd
-from models import *
-from database.connection import session
+from prepare_data_2 import formating_df
+from init_db_func import create_db_objects, read_csv
 from clear_data import clear_all
-from models.date import get_date
 
-fields = ['nkill', 'nwound', 'nperps', 'longitude', 'latitude', 'attacktype1_txt',
-          'country_txt', 'city', 'iyear', 'imonth', 'iday', 'region_txt', 'gname', 'targtype1_txt']
+def init_db(file_path, fields, need_prepare=False):
+    data = read_csv(file_path, fields)
+    if need_prepare:
+        data = formating_df(data)
+    clear_data = clear_all(data)
 
-df_ = pd.read_csv('../data/globalterrorismdb.csv',
-                 skipinitialspace=True,
-                 usecols=fields,
-                 encoding='latin1') \
-        .drop_duplicates(keep='first', inplace=False)
-
-df = clear_all(df_)
-
-def get_or_create(table_class, search_fields, create_func):
-    obj = session.query(table_class).filter_by(**search_fields).first()
-
-    if obj:
-        return obj
-    else:
-        try:
-            new_obj = create_func()
-            add_objects_to_db(new_obj)
-            return new_obj
-        except Exception as e:
-            print(f"Error creating {table_class.__name__}: {e}")
-            return None
-
-
-def create_db_objects(data):
-    attack_type_obj = get_or_create(AttackType, {'attack_type': data['attacktype1_txt']}, lambda: create_attack_type_obj(data))
-    attack_type_id = attack_type_obj.att_type_id
-
-    date_obj = get_or_create(DateModel, {'date': get_date(data)}, lambda: create_date_obj(data))
-    date_id = date_obj.date_id
-
-    location_obj = get_or_create(Location, {
-        'country': data['country_txt'],
-        'city': data['city'],
-        'region': data['region_txt']
-    }, lambda: create_location_obj(data))
-    location_id = location_obj.loc_id
-
-    target_type_obj = get_or_create(TargetType, {'target_type': data['targtype1_txt']}, lambda: create_target_type_obj(data))
-    target_type_id = target_type_obj.target_type_id
-
-    terror_group_obj = get_or_create(TerrorGroup, {'gang_name': data['gname']}, lambda: create_terror_group_obj(data))
-    terror_group_id = terror_group_obj.gang_id
-
-    attack_obj = create_attack_obj(data, target_type_id, attack_type_id, location_id, date_id, terror_group_id)
-    add_objects_to_db(attack_obj)
-
-    return attack_type_id, date_id, location_id, target_type_id, terror_group_id, attack_obj
-
-def add_objects_to_db(*objects):
-    try:
-        for obj in objects:
-            session.add(obj)
-        session.commit()
-    except Exception as e:
-        session.rollback()
-        print(f"Error adding objects to session: {e}")
-
-def init_db(data):
     counter = 0
     try:
-        for index, row in data.iterrows():
+        for index, row in clear_data.iterrows():
             attack_type_id, date_id, location_id, target_type_id, terror_group_id, attack_obj = create_db_objects(row)
             if attack_type_id and date_id and location_id and target_type_id and terror_group_id and attack_obj:
                 counter += 1
@@ -78,5 +21,12 @@ def init_db(data):
     except Exception as e:
         print(f"Error processing message: {e}")
 
-init_db(df)
+fields_data_1 = ['nkill', 'nwound', 'nperps', 'longitude', 'latitude', 'attacktype1_txt',
+          'country_txt', 'city', 'iyear', 'imonth', 'iday', 'region_txt', 'gname', 'targtype1_txt']
+data_path_1 = '../data/globalterrorismdb_0718dist-1000 rows.csv'
 
+fields_data_2 = ['Date', 'City', 'Country',	'Perpetrator', 'Weapon', 'Injuries', 'Fatalities', 'Description']
+data_path_2 = '../data/RAND_Database_of_Worldwide_Terrorism_Incidents - 5000 rows.csv'
+
+#init_db(data_path_2, fields_data_2, need_prepare=True)
+#init_db(data_path_1, fields_data_1, need_prepare=False)
